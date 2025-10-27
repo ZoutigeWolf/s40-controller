80f4445cb3971313d6bb3957932cef5f970266ae
ZoutigeWolf <guuskamphuis@gmail.com>
2025-10-27 20:41:10 +0100

    shit

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI, Response
 from camilladsp import CamillaClient, CamillaError
-from gpiozero import RotaryEncoder, Button
 
 load_dotenv()
 
@@ -10,26 +9,6 @@ app = FastAPI()
 
 camilla = CamillaClient(os.getenv("CAMILLA_HOST"), int(os.getenv("CAMILLA_PORT")))
 camilla.connect()
-
-encoder = RotaryEncoder(a=9, b=10, max_steps=0)
-button = Button(11)
-
-def on_encoder_rotate_cw():
-    volume = camilla.volume.main_volume()
-    camilla.volume.set_main_volume(volume + 1)
-
-def on_encoder_rotate_ccw():
-    volume = camilla.volume.main_volume()
-    camilla.volume.set_main_volume(volume - 1)
-
-def on_encoder_press():
-    mute = camilla.volume.main_mute()
-    camilla.volume.set_main_mute(not mute)
-
-encoder.when_rotated_clockwise = on_encoder_rotate_cw
-encoder.when_rotated_counter_clockwise = on_encoder_rotate_ccw
-button.when_activated = on_encoder_press
-
 
 @app.get("/config")
 async def get_config():
@@ -56,3 +35,21 @@ async def get_devices():
         "capture": {t: [d[0] for d in camilla.general.list_capture_devices(t)] for t in types[1]},
         "playback": {t: [d[0] for d in camilla.general.list_capture_devices(t)] for t in types[0]}
     }
+
+
+@app.post("/control/volume-up")
+async def control_volume_up():
+    volume = camilla.volume.main_volume()
+    camilla.volume.set_main_volume(volume + 1)
+
+
+@app.post("/control/volume-down")
+async def control_volume_down():
+    volume = camilla.volume.main_volume()
+    camilla.volume.set_main_volume(volume - 1)
+
+
+@app.post("/control/mute")
+async def control_mute():
+    mute = camilla.volume.main_mute()
+    camilla.volume.set_main_mute(not mute)

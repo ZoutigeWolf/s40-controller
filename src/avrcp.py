510965c648dd9aa1df00cff4e969053c58635a2d
ZoutigeWolf <guuskamphuis@gmail.com>
2025-10-31 02:42:26 +0100

    shit

--- src/avrcp.py
+++ src/avrcp.py
@@ -5,8 +5,6 @@ class AVRCPClient:
         self.bus = SystemBus()
         self.manager = self.bus.get("org.bluez", "/")
         self.player = self._discover_player()
-        if self.player is None:
-            raise RuntimeError("No AVRCP device connected.")
 
     def _discover_player(self):
         objects = self.manager.GetManagedObjects()
@@ -15,19 +13,12 @@ class AVRCPClient:
                 return self.bus.get("org.bluez", path)
         return None
 
-    def play(self):
-        self.player.Play()
-
-    def pause(self):
-        self.player.Pause()
-
-    def next(self):
-        self.player.Next()
-
-    def previous(self):
-        self.player.Previous()
-
     def get_current(self):
+        self.player = self._discover_player()
+
+        if self.player is None:
+            return None
+
         return {
             "status": self.player.Status,
             "track": self.player.Track,
